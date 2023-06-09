211eb23776b8067d461eb7bc2a19dc8edde4c538
Eileenpngg <eileenpng1843@gmail.com>
2023-06-09 17:15:41 +0800

    added handleAdd

--- ToDoList/src/App.tsx
+++ ToDoList/src/App.tsx
@@ -1,14 +1,29 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import ToDoForm from "./components/ToDoForm";
 import ToDoList from "./components/ToDoList";
 
+import { ToDo } from "./model";
+
 const App: React.FC = () => {
   const [toDo, setToDo] = useState<string>("");
+  const [toDoList, setToDoList] = useState<ToDo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (toDo) {
+      setToDoList([
+        ...toDoList,
+        { id: Date.now(), task: toDo, completed: false },
+      ]);
+      setToDo("");
+    }
+  };
+  console.log(toDoList);
   return (
     <>
-      <ToDoForm toDo={toDo} setToDo={setToDo} />
+      <ToDoForm toDo={toDo} setToDo={setToDo} handleAdd={handleAdd} />
       <ToDoList />
     </>
   );
--- ToDoList/src/components/ToDoForm.tsx
+++ ToDoList/src/components/ToDoForm.tsx
@@ -1,21 +1,26 @@
+
+
 interface toDoProps {
   toDo: string;
   setToDo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const ToDoForm: React.FC<toDoProps> = ({ toDo, setToDo }) => {
+const ToDoForm: React.FC<toDoProps> = ({ toDo, setToDo, handleAdd }) => {
   return (
     <>
-      <div id="toDoForm">
-        <label htmlFor="todoinput">Create a ToDo 📝</label>
-        <input
-          placeholder="e.g Sweep the floor"
-          id="todoinput"
-          size={40}
-          value={toDo}
-          onChange={(e) => setToDo(e.target.value)}
-        />
-        <button id="addToDo">Add To Do</button>
-      </div>
+      <form className="toDoInput" onSubmit={handleAdd}>
+        <div id="toDoForm">
+          <label htmlFor="todoinput">Create a ToDo 📝</label>
+          <input
+            placeholder="e.g Sweep the floor"
+            id="todoinput"
+            size={40}
+            value={toDo}
+            onChange={(e) => setToDo(e.target.value)}
+          />
+          <button id="addToDo">Add To Do</button>
+        </div>
+      </form>
     </>
   );
 };
